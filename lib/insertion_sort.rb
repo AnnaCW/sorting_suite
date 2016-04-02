cbf8ebf727a28189afad43c686a7a19153f91e87
Anna Weisbrodt <aweisbro@gmail.com>
2016-04-01 23:48:35 -0600

    major refactor - recursive, divided sort into 3 methods

--- lib/insertion_sort.rb
+++ lib/insertion_sort.rb
@@ -1,43 +1,66 @@
+require "pry"
+
 class InsertionSort
-  attr_reader :sorted
+  attr_accessor :sorted_array
+  attr_accessor :unsorted_element
+  attr_accessor :unsorted_array
 
   def initialize
-    @sorted = []
+    @sorted_array=[]
+    @unsorted_element
+    @unsorted_array
   end
 
     #INSERT inserts BEFORE the element with the given index
 
-  def sort(unsorted)
-    i = 0
-    u = unsorted[0]
+  def sort(unsorted_array, i=0)
+    unsorted_element = unsorted_array[0]
+    @unsorted_array = unsorted_array
+    ##NEED TO DEFINE I
 
-    if sorted.empty?
-      sorted.insert(1, u)
-      unsorted.delete(u)
+    if unsorted_array.empty?
+      return sorted_array
 
-    elsif u < sorted[0]
-      sorted.insert(1, u)
-      unsorted.delete(u)
+    elsif sorted_array.empty?
+      move_element(i, unsorted_element, unsorted_array)
+      sort(unsorted_array, i)
 
-    else
-      # u > sorted[i]
-      # keep checking to the right
-      until u < sorted[i] || i == (sorted.length-1)
-        i += 1
-      end
-      sorted.insert(i+1, u)
-      unsorted.delete(u)
+    elsif unsorted_element < sorted_array[i]
+      move_element(i, unsorted_element)
+      sort(unsorted_array, i)
+
+    elsif unsorted_element > sorted_array[i]
+      check_right(unsorted_element, i+=1)
 
     end
-    p unsorted
-    p sorted
+    return sorted_array
   end
 
 
+  def move_element(i, unsorted_element, unsorted_array)
+    sorted_array.insert(i, unsorted_element)
+    unsorted_array.drop(1)
+    # return sorted_array
+  end
+
+
+      #added 1 to i before passing to check right
+  def check_right(unsorted_element, i)
+      if sorted_array[i].nil
+        move_element(i, unsorted_element)
+
+      elsif unsorted_element < sorted_array[i]
+        move_element(i, unsorted_element)
+
+      elsif unsorted_element > sorted_array[i]
+        check_right(unsorted_element, i+=1)
+      end
+  end
+
 
 end
+  #class
 
-#edge case - empty unsorted array - should return empty sorted
 
 sorter = InsertionSort.new
-sorter.sort(["d", "b", "a", "c"])
+p sorter.sort(["d", "b", "a", "e"])
