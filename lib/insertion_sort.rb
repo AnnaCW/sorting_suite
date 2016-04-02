5237d2f4528fb6d0e044483873d60619d8687a68
Anna Weisbrodt <aweisbro@gmail.com>
2016-04-02 11:18:59 -0600

    refactor, added each loop. This one worksgit add insertion_sort.rb

--- lib/insertion_sort.rb
+++ lib/insertion_sort.rb
@@ -1,62 +1,83 @@
 require "pry"
 
 class InsertionSort
-  attr_accessor :sorted_array
-  attr_accessor :unsorted_element
-  attr_accessor :unsorted_array
+  # attr_accessor :sorted_array
+  # attr_accessor :unsorted_element
+  # attr_accessor :unsorted_array
 
-  def initialize
-    @sorted_array=[]
-    @unsorted_element
-    @unsorted_array
-  end
+  # def initialize
+  #   # @sorted_array=[]
+  #   @unsorted_element
+  #   @unsorted_array
+  # end
 
     #INSERT inserts BEFORE the element with the given index
 
-  def sort(unsorted_array, i=0)
-    unsorted_element = unsorted_array[0]
-    @unsorted_array = unsorted_array
-    ##NEED TO DEFINE I
+  def sort(unsorted_array, sorted_array=[], index=0)
+    # unsorted_element = unsorted_array[0]
+
+    unsorted_array.each do |unsorted_element|
+      index = 0
 
     if unsorted_array.empty?
       return sorted_array
 
     elsif sorted_array.empty?
-      move_element(i, unsorted_element, unsorted_array)
-      sort(unsorted_array, i)
 
-    elsif unsorted_element < sorted_array[i]
-      move_element(i, unsorted_element)
-      sort(unsorted_array, i)
+      sorted_array.insert(index=0, unsorted_element)
+      unsorted_array.drop(1)
 
-    elsif unsorted_element > sorted_array[i]
-      check_right(unsorted_element, i+=1)
+      # sort(unsorted_array, sorted_array, index)
+        #PROCEED TO NEXT UNSORTED ELEMENT
+        #INDEX DOESN'T CHANGE
+
+    elsif unsorted_element < sorted_array[index]
+
+      sorted_array.insert(index, unsorted_element)
+      unsorted_array.drop(1)
+
+      # sort(unsorted_array, sorted_array, index)
+        #PROCEED TO NEXT UNSORTED ELEMENT
+        #INDEX DOESN'T CHANGE
+
+    elsif unsorted_element > sorted_array[index]
+      check_right(unsorted_element, sorted_array, index+=1, unsorted_array)
+
+      # sort(unsorted_array, sorted_array, index)
 
     end
-    return sorted_array
+    #end if/else
   end
+    #end for each
 
-
-  def move_element(i, unsorted_element, unsorted_array)
-    sorted_array.insert(i, unsorted_element)
-    unsorted_array.drop(1)
-    # return sorted_array
+  sorted_array
   end
-
 
       #added 1 to i before passing to check right
-  def check_right(unsorted_element, i)
-      if sorted_array[i].nil
-        move_element(i, unsorted_element)
+  def check_right(unsorted_element, sorted_array, index, unsorted_array)
 
-      elsif unsorted_element < sorted_array[i]
-        move_element(i, unsorted_element)
+      if sorted_array[index].nil?
 
-      elsif unsorted_element > sorted_array[i]
-        check_right(unsorted_element, i+=1)
+        sorted_array.insert(index, unsorted_element)
+        unsorted_array.drop(1)
+
+      elsif unsorted_element < sorted_array[index]
+
+        sorted_array.insert(index, unsorted_element)
+        unsorted_array.drop(1)
+
+      elsif unsorted_element > sorted_array[index]
+        check_right(unsorted_element, sorted_array, index+=1, unsorted_array)
       end
+
+    sorted_array
   end
 
+  # def move_element(i, unsorted_element, unsorted_array)
+  #   sorted_array.insert(i, unsorted_element)
+  #   unsorted_array.drop(1)
+  #   return sorted_array
+  # end
 
 end
   #class
