94294cffaf82fa8a4df71e78201b0d7fee794f59
Anna Weisbrodt <aweisbro@gmail.com>
2016-04-02 12:52:20 -0600

    insertion sort not in place created by modifying insertion sort

---
+++ lib/insertion_sort_not_in_place.rb
@@ -0,0 +1,49 @@
+
+class InsertionSortNotInPlace
+
+  def sort(unsorted_array, sorted_array=[], index=0)
+    unsorted_element = unsorted_array[0]
+    index=0
+
+      if unsorted_array.empty?
+        return sorted_array
+
+      elsif sorted_array.empty?
+        move_element(sorted_array, index, unsorted_element, unsorted_array)
+        sort(unsorted_array, sorted_array, index)
+
+      elsif unsorted_element < sorted_array[index]
+        move_element(sorted_array, index, unsorted_element, unsorted_array)
+        sort(unsorted_array, sorted_array, index)
+
+      elsif unsorted_element > sorted_array[index]
+        check_right(unsorted_element, sorted_array, index+=1, unsorted_array)
+        sort(unsorted_array, sorted_array, index)
+      end
+
+    sorted_array
+  end
+
+
+  def check_right(unsorted_element, sorted_array, index, unsorted_array)
+    if sorted_array[index].nil?
+        move_element(sorted_array, index, unsorted_element, unsorted_array)
+
+    elsif unsorted_element < sorted_array[index]
+        move_element(sorted_array, index, unsorted_element, unsorted_array)
+
+    elsif unsorted_element > sorted_array[index]
+        check_right(unsorted_element, sorted_array, index+=1, unsorted_array)
+    end
+    sorted_array
+  end
+
+  def move_element(sorted_array, index, unsorted_element, unsorted_array)
+    sorted_array.insert(index, unsorted_element)
+    unsorted_array.shift
+  end
+
+end
+
+# sorter = InsertionSortNotInPlace.new
+# p sorter.sort(["d", "b", "a", "e"])
