102187ebb8a8798ddda27305c1bcafb664b9cd3b
Anna Weisbrodt <aweisbro@gmail.com>
2016-04-02 21:21:20 -0600

    added tests - basic tests finished

--- test/bubble_sort_test.rb
+++ test/bubble_sort_test.rb
@@ -10,3 +10,54 @@ class BubbleSortTest < Minitest::Test
 
     assert_equal BubbleSort, sorter.class
   end
+
+  def test_sort_empty_array
+    sorter = BubbleSort.new
+    sorter.sort([""])
+
+    expected = [""]
+    assert_equal expected, sorter.sort([""])
+  end
+
+  def test_sort_1_element_array
+    sorter = BubbleSort.new
+    sorter.sort(["c"])
+
+    expected = ["c"]
+    assert_equal expected, sorter.sort(["c"])
+  end
+
+  def test_sort_3_element_array
+    sorter = BubbleSort.new
+    sorter.sort(["c", "h", "a"])
+
+    expected = ["a", "c", "h"]
+    assert_equal expected, sorter.sort(["c", "h", "a"])
+  end
+
+  def test_large_array_of_numbers
+    sorter = BubbleSort.new
+    sorter.sort([4, 7, 3, 5, 9, 1, 67, 22])
+
+    expected = [1, 3, 4, 5, 7, 9, 22, 67]
+    assert_equal expected, sorter.sort([4, 7, 3, 5, 9, 1, 67, 22])
+  end
+
+  def test_of_numbers_with_zero
+    sorter = BubbleSort.new
+    sorter.sort([4, 7, 3, 0, 9])
+
+    expected = [0, 3, 4, 7, 9]
+    assert_equal expected, sorter.sort([4, 7, 3, 0, 9])
+  end
+
+  def test_of_numbers_with_negatives
+    sorter = BubbleSort.new
+    sorter.sort([4, 7, -2, 0, -4])
+
+    expected = [-4, -2, 0, 4, 7]
+    assert_equal expected, sorter.sort([4, 7, -2, 0, -4])
+  end
+
+
+end
