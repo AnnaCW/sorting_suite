92ea8d9b2c0ae7abc39399ee28ba1d61b8af7aa7
Anna Weisbrodt <aweisbro@gmail.com>
2016-04-02 21:06:07 -0600

    finished v1, works for 4 elements

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -4,8 +4,9 @@ class MergeSort
     left_holding = []
     right_holding = []
 
-    if arr.length <= 2
+    if arr.length < 2
       sorted_array << arr
+      return sorted_array
 
     else
       split_at = arr.length/2
@@ -18,10 +19,57 @@ class MergeSort
       split_right(right)
         right_holding = right
 
+      left_sorted = sort_left(left_holding)
+      right_sorted = sort_right(right_holding)
+
+      merge(left_sorted, right_sorted, sorted_array)
+
     end
-    p left_holding
-    p right_holding
-    sorted_array
+    p sorted_array
+  end
+
+  def sort_left(left_holding)
+    if left_holding.length == 1 || left_holding[0] < left_holding[1]
+      left_sorted = left_holding
+    else
+      left_sorted = left_holding[1], left_holding[0]
+    end
+    left_sorted
+  end
+
+  def sort_right(right_holding)
+    if right_holding.length == 1 || right_holding[0] < right_holding[1]
+      right_sorted = right_holding
+    else
+      right_sorted = right_holding[1], right_holding[0]
+    end
+    right_sorted
+  end
+
+
+  def merge(left_sorted,right_sorted,sorted_array)
+     if left_sorted.empty? && right_sorted.empty?
+       return sorted_array
+
+     elsif right_sorted.empty?
+       sorted_array << left_sorted[0]
+       return sorted_array
+
+     elsif left_sorted.empty?
+       sorted_array << right_sorted[0]
+       return sorted_array
+
+     elsif left_sorted[0] < right_sorted[0]
+       sorted_array << left_sorted[0]
+       left_sorted.delete_at(0)
+       merge(left_sorted, right_sorted, sorted_array)
+
+     elsif right_sorted[0] < left_sorted[0]
+       sorted_array << right_sorted[0]
+       right_sorted.delete_at(0)
+       merge(left_sorted, right_sorted, sorted_array)
+     end
+  sorted_array
   end
 
 
@@ -53,4 +101,4 @@ end
 
 
 sorter = MergeSort.new
-sorter.sort([6, 5, 3, 7, 2])
+sorter.sort([6, 5, 3, 7])
