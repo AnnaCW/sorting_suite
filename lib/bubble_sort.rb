d490659d25d9e92dcd9a5fe87f943faee79679db
Anna Weisbrodt <aweisbro@gmail.com>
2016-04-03 20:41:00 -0600

    updated to handle duplicates

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -6,7 +6,7 @@ class BubbleSort
     loop do
       clean_pass=true
       for i in 1 .. x do
-        if arr[i] < arr[i-1]
+        if arr[i] <= arr[i-1]
           arr[i-1], arr[i] = arr[i], arr[i-1]
           clean_pass=false
         end
@@ -16,15 +16,10 @@ class BubbleSort
         break
       end
     end
-    p arr
+    arr
   end
 
 end
 
-
 sorter = BubbleSort.new
-sorter.sort([3, 4, 9, 1, 6])
-
-# => #<BubbleSort:0x007f81a19e94e8>
 sorter.sort(["d", "b", "a", "c"])
-# => ["a", "b", "c", "d"]
